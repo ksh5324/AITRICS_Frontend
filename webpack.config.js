e183b5ac7fb2974f862fa661b74273c8713423c0
강성훈 <yolly050101@naver.com>
2023-05-26 08:41:38 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    "react-vendors": ["react", "react-dom", "react-router-dom"],
+    main: {
+      import: "./src/index.tsx",
+      dependOn: "react-vendors",
+    },
+  },
+  output: {
+    chunkLoadTimeout: 3000,
+    filename: "[name].bundle.[contenthash].js",
+    charset: true,
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[contenthash][ext]",
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_module/,
+        use: ["swc-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [{ loader: "file-loader" }],
+        // type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".js", ".ts", ".tsx", ".png"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 3080,
+  },
+};
