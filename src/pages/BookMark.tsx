50d7822e349c74477562639678516d4eda1cea73
강성훈 <yolly050101@naver.com>
2023-05-31 10:49:40 +0900

    [FEAT] bookmark

--- src/pages/BookMark.tsx
+++ src/pages/BookMark.tsx
@@ -1,34 +1,8 @@
 import React from "react";
-import Table from "../components/common/Table";
-import { TableContext } from "../components/PatientTable";
-import usePatientTable from "../components/PatientTable/hooks/usePatientTable";
-import { bookMarkPatientHeaderData } from "../constant/bookMarkPatientConstant";
-import { BookMarkStorageConvertor } from "../lib/convertor/bookmarkStorageConvertor";
+import BookMarkTable from "../components/BookMarkTable";
 
 const BookMark = () => {
-  const bookMarkPatientTableValue = usePatientTable();
-  return (
-    <>
-      <Table Context={TableContext} value={bookMarkPatientTableValue}>
-        <Table.Header
-          Context={TableContext}
-          header={bookMarkPatientHeaderData}
-          bookmark
-        />
-        <Table.Body>
-          {localStorage.getItem("bookmark") &&
-            BookMarkStorageConvertor().map((v) => (
-              <Table.Row
-                bookmark="boomark"
-                Context={TableContext}
-                item={v}
-                key={v.patientNum}
-              />
-            ))}
-        </Table.Body>
-      </Table>
-    </>
-  );
+  return <BookMarkTable />;
 };
 
 export default BookMark;
