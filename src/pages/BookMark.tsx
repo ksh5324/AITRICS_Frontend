48f9905c5283549b2b2492144b55bc4eccecd63a
강성훈 <yolly050101@naver.com>
2023-05-31 10:47:35 +0900

    [FEAT] bookmark

--- src/pages/BookMark.tsx
+++ src/pages/BookMark.tsx
@@ -2,25 +2,8 @@ import React from "react";
 import Table from "../components/common/Table";
 import { TableContext } from "../components/PatientTable";
 import usePatientTable from "../components/PatientTable/hooks/usePatientTable";
-import { PatientTableField } from "../components/PatientTable/types";
+import { bookMarkPatientHeaderData } from "../constant/bookMarkPatientConstant";
 import { BookMarkStorageConvertor } from "../lib/convertor/bookmarkStorageConvertor";
-
-const bookMarkPatientHeaderData: Record<
-  PatientTableField,
-  { value: string; sort: boolean }
-> = Object.freeze({
-  state: { value: "상태", sort: false },
-  patient: { value: "개인정보", sort: false },
-  patientNum: { value: "환자번호", sort: false },
-  location: { value: "위치", sort: false },
-  date: { value: "입원일", sort: false },
-  department: { value: "해당과", sort: false },
-  doctorInCharge: { value: "주치의", sort: false },
-  diagnosis: { value: "진단명", sort: false },
-  type: { value: "관측 데이터 종류", sort: false },
-  value: { value: "관측 데이터", sort: false },
-  observedAt: { value: "관측 일자", sort: false },
-});
 
 const BookMark = () => {
   const bookMarkPatientTableValue = usePatientTable();
